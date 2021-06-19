47fc70619db8a20afe9ab155ba49e5fdf556c7da
Pumudu-anuradha <85781602+Pumudu-anuradha@users.noreply.github.com>
2021-06-19 17:47:22 +0530

    Add files via upload

---
+++ stickers1.py
@@ -0,0 +1,69 @@
+from userbot.utils import admin_cmd 
+ 
+ 
+ 
+ 
+@borg.on(admin_cmd(pattern="1gm$")) 
+ 
+ 
+ 
+async def gn(event):
+ 
+ 
+ 
+    await event.edit(" very good morning 🙂 to day are very beautiful day ❤️")
+
+@borg.on(admin_cmd(pattern="mk$")) 
+ 
+ 
+ 
+async def gn(event):
+ 
+ 
+ 
+    await event.edit(" hi brother🙂 what are you doing?")
+
+import asyncio
+
+@ultroid_cmd(pattern="jok")
+async def naruto(ult):
+
+ await ult.edit("Hello, I'm coammdo ✊")
+ await asyncio.sleep(1)
+ await ult.edit("😆")
+ await asyncio.sleep(3)
+ await ult.edit("🤣")
+ await asyncio.sleep(2)
+ await ult.edit("don't worry brother 😆😆😆😆😆😆")
+ await asyncio.sleep(1)
+ await ult.edit(" 😁")
+
+
+
+@ultroid_cmd(pattern="nng")
+async def naruto(ult):
+
+ await ult.edit("Hello, I'm ninja")
+ await asyncio.sleep(1)
+ await ult.edit("🔥")
+ await asyncio.sleep(3)
+ await ult.edit("⚡️")
+ await asyncio.sleep(2)
+ await ult.edit("🌚")
+ await asyncio.sleep(1)
+ await ult.edit(" 🙃")
+
+
+
+@ultroid_cmd(pattern="hand")
+async def naruto(ult):
+
+ await ult.edit("👉")
+ await asyncio.sleep(1)
+ await ult.edit("👉👈")
+ await asyncio.sleep(3)
+ await ult.edit("🎃")
+ await asyncio.sleep(2)
+ await ult.edit("☝️")
+ await asyncio.sleep(1)
+ await ult.edit("🤤")
